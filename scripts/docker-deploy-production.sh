27491c55b985aa4f497f151269c49b2b4cf7ad2b
DuJust <du.just.it@gmail.com>
2017-02-17 03:43:49 +0800

    update deploy script.

--- scripts/docker-deploy-production.sh
+++ scripts/docker-deploy-production.sh
@@ -11,6 +11,7 @@ DB_USER='root'
 DB_PASSWORD='secret'
 DB_HOST="${APP_NAME}_db"
 SECRET_KEY_BASE="19ceb28a243e6c86e7ddf73dac35cdfc6db487bcf302d7ae448ceecc65fea97130d7d7ead4ea98cede42ae0c6bc849f7aedf8284e784801098006b40bf1a1513"
+RAILS_SERVE_STATIC_FILES="true"
 
 app_container_id=$(docker ps -a -q -f name=${APP_NAME})
 if [[ ${app_container_id} ]]; then
@@ -48,7 +49,7 @@ else
 fi
 
 echo "Running a new ${APP_NAME} container..."
-docker run --name ${APP_NAME} --link ${DB_HOST}:${DB_HOST} -e DB_USER=${DB_USER} -e DB_PASSWORD=${DB_PASSWORD} -e DB_HOST=${DB_HOST} -e DB_NAME=${APP_NAME} -e SECRET_KEY_BASE=${SECRET_KEY_BASE} -e RAILS_ENV=production -d ${APP_NAME}
+docker run --name ${APP_NAME} --link ${DB_HOST}:${DB_HOST} -e DB_USER=${DB_USER} -e DB_PASSWORD=${DB_PASSWORD} -e DB_HOST=${DB_HOST} -e DB_NAME=${APP_NAME} -e SECRET_KEY_BASE=${SECRET_KEY_BASE} -e RAILS_SERVE_STATIC_FILES=${RAILS_SERVE_STATIC_FILES} -e RAILS_ENV=production -d ${APP_NAME}
 
 echo "Building a new ${APP_NGINX} image..."
 docker build -t ${APP_NGINX} -f docker/nginx/Nginx-Dockerfile docker/nginx
